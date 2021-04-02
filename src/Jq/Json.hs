b7a2ba0a3a53780e663c43ec652f18fe08dec6e9
Mihhail Sokolov <m.sokolov@student.tudelft.nl>
2021-04-02 19:58:48 +0200

    Fix showing unicode characters

--- src/Jq/Json.hs
+++ src/Jq/Json.hs
@@ -1,5 +1,7 @@
 module Jq.Json where
 
+import Text.Show.Unicode
+
 data JSON = JNull | JString String | JBool Bool | JArray [JSON] | JObject [(String, JSON)] | JChar Char | JNum Double
   deriving (Eq)
 
@@ -9,7 +11,7 @@ instance Show JSON where
 prettyPrint :: Int -> JSON -> String
 prettyPrint _ JNull = "null"
 prettyPrint _ (JNum jn) = printNum jn
-prettyPrint _ (JString str) = show str
+prettyPrint _ (JString str) = ushow str
 prettyPrint _ (JChar char) = "\"" ++ [char] ++ "\""
 prettyPrint _ (JBool b) = if b then "true" else "false"
 prettyPrint n (JArray arr) = if length arr > 0 then "[\n" ++ prettyPrintArray (n + 2) arr ++ "\n" ++ indentation n ++ "]" else "[]"
